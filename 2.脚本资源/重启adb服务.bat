49db541d5cc8d51fc9c5fd98981058f77ee98e55
hui-shao <31568606+hui-shao@users.noreply.github.com>
2018-08-17 23:21:49 +0800

    优化程序列表显示
    把占用5037 端口的程序列表输出到txt。看得更清楚

--- 2.脚本资源/重启adb服务.bat
+++ 2.脚本资源/重启adb服务.bat
@@ -43,6 +43,8 @@ mode con lines=36 cols=80
 cd /d %~dp0
 ECHO. ==============================================================
 ECHO. 重启adb服务
+echo.
+echo 请务必先退出 手机助手类程序
 ECHO.
 ECHO. 按任意键继续
 ECHO. ==============================================================
@@ -55,7 +57,8 @@ ECHO. 正在尝试重启ADB服务~
 ECHO.
 ECHO. ====================================================
 ECHO.
-netstat -ano |findstr "5037"
+netstat -ano |findstr "5037" > %~dp0plist.txt
+start %~dp0plist.txt
 echo.
 set pid=
 set /p pid= 输入LISTENING 后面的数字(没有就空着),然后后回车:
@@ -67,7 +70,9 @@ echo 结束进程失败，请在任务管理器手动结束后按任意键继续
 echo 进程名在“%pid%”的前面,一般为手机助手类&pause >nul
 
 :T
+del /f /q %~dp0plist.txt >nul
 echo.
+taskkill /f /im adb.exe
 adb kill-server
 adb start-server
 ping 127.0.0.1 /n 3 >nul
